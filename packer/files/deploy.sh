7fdc755c215d8a70f9c43309865e07fc1629dc0d
Demenev Maksim <6254918+v1k3ng@users.noreply.github.com>
2019-03-28 12:29:21 +0500

    Changed deploy.sh

--- packer/files/deploy.sh
+++ packer/files/deploy.sh
@@ -12,7 +12,7 @@ bundle install
 
 # start server
 #puma -d
-wget https://raw.githubusercontent.com/otus-devops-2019-02/v1k3ng_infra/packer-base/packer/files/puma.service -O /etc/systemd/system/puma.service
+sudo wget https://raw.githubusercontent.com/otus-devops-2019-02/v1k3ng_infra/packer-base/packer/files/puma.service -O /etc/systemd/system/puma.service
 sudo systemctl daemon-reload 
 sudo systemctl enable puma.service
 

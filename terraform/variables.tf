0f137ff5714a28e61db08a9c5ea2c18f1c3f7748
Demenev Maksim <6254918+v1k3ng@users.noreply.github.com>
2019-04-01 16:11:26 +0500

    After terraform fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,28 @@
 variable project {
-	description = "Project ID"
-}
-variable region {
-	description = "Region"
-# Значение по умолчанию
-	default = "europe-west1"
-}
-variable public_key_path {
-# Описание переменной
-	description = "Path to the public key used for ssh access"
-}
-variable disk_image {
-	description = "Disk image"
-}
-variable private_key_path {
-	description = "Private key"
-}
-variable zone {
-	description = "Zone"
-	default = "europe-west1-b" 
+  description = "Project ID"
 }
 
+variable region {
+  description = "Region"
+
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable private_key_path {
+  description = "Private key"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}

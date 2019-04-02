711b31a225792d2cfb88c40ec8c88dd84b5cdd17
Demenev Maksim <6254918+v1k3ng@users.noreply.github.com>
2019-04-02 11:47:36 +0500

    Added ssh-keys to project metadata (page 52)

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,15 @@ provider "google" {
   region  = "${var.region}"
 }
 
+resource "google_compute_project_metadata" "default" {
+  project = "${var.project}"
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+    ssh-keys = "appuser1:${file(var.public_key_path)}"
+  }
+}
+
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"

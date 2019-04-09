257ab5542750473ee1466ccd34a3c45cc332bf64
Demenev Maksim <6254918+v1k3ng@users.noreply.github.com>
2019-04-09 11:28:26 +0500

    Changed count to 1, moved lb.tf to files/, commented target_pool_ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "google_compute_instance" "app" {
   name         = "reddit-app${count.index}"
   machine_type = "g1-small"
   zone         = "${var.zone}"
-  count        = "2"
+  count        = "1"
 
   # определение загрузочного диска
   boot_disk {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,6 @@ output "apps_external_ip" {
   value = "${google_compute_instance.app.*.network_interface.0.access_config.0.nat_ip}"
 }
 
-output "target_pool_ip" {
-  value = "${google_compute_forwarding_rule.default.ip_address}"
-}
+// output "target_pool_ip" {
+//   value = "${google_compute_forwarding_rule.default.ip_address}"
+// }

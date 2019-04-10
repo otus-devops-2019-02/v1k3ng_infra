82508fa5564423c4bfd4f111886e49444f48e486
Demenev Maksim <6254918+v1k3ng@users.noreply.github.com>
2019-04-10 10:37:28 +0500

    Up to page 19

--- terraform/modules/app/outputs.tf
+++ terraform/modules/app/outputs.tf
@@ -2,8 +2,8 @@
 //   value = "${google_compute_instance.app0.network_interface.0.access_config.0.nat_ip}"
 // }
 
-output "app_external_ip" {
-  value = "${google_compute_instance.db.network_interface.0.access_config.0.nat_ip}"
+output "db_external_ip" {
+  value = "${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
 }
 
 // output "target_pool_ip" {
